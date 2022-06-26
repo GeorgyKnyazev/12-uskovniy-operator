180b6ff6d026c2fdae5ac141322e0c5bd659a4aa
Георгий Князев <saint_georgy@yahoo.com>
2022-06-26 15:32:07 +0300

    1

--- 12 uskovniy operator/Program.cs
+++ 12 uskovniy operator/Program.cs
@@ -23,23 +23,21 @@ namespace _12_uskovniy_operator
             {
                 Console.WriteLine($"Жизни игрока: {playerHealth}, Жизни Босса: {bossHealth}, Вызван ли дух: {isSpiritSummoned}, Можно скрыться: {numberOfDeparturesInFult}" );
                 Console.WriteLine("Выберите заклинание");
-                Console.WriteLine("1 - Удар (Наносит 150 ед. урона)");
-                Console.WriteLine("2 - Решамон (Призыв теневого духа. отнимает 100 ед. здоровья игроку)");
-                Console.WriteLine("3 - Хуганзакура (Доступна после вызова духа. Наносит 300 ед. урона)");
-                Console.WriteLine($"4 - Межпространственный разлом (Позволяет скрыться в разломе от урона противника и востановить 200 ед. здоровья.\n доступно: {numberOfDeparturesInFult}) ");
+                Console.WriteLine($"1 - Удар (Наносит {playerAndSpiritDamage} ед. урона)");
+                Console.WriteLine($"2 - Решамон (Призыв теневого духа. отнимает {lifeForSummoningOfSpirit} ед. здоровья игроку)");
+                Console.WriteLine($"3 - Хуганзакура (Доступна после вызова духа. Наносит {playerAndSpiritDamage} ед. урона)");
+                Console.WriteLine($"4 - Межпространственный разлом (Позволяет скрыться в разломе от урона противника и востановить {increasedHealthInRift} ед. здоровья.\n доступно: {numberOfDeparturesInFult}) ");
                 userInput = Console.ReadLine();
 
                 switch (userInput)
                 {
-                    case "1":
-                        {
+                    case "1":                        
                             Console.WriteLine($"Вы нанесли {playerStrikeDamage} ед. урона Боссу. Босс вам нанес {bossDamage} ед. урона");
                             playerHealth -= bossDamage;
                             bossHealth -= playerStrikeDamage;
                             break;
-                        }
+                        
                     case "2":
-                        {
                             if (isSpiritSummoned == false)
                             {
                                 Console.WriteLine($"Вы призвали теневого духа из отдали за это {lifeForSummoningOfSpirit} ед. здоровья.\n Получили от Босса: {bossDamage} ед. урона ");
@@ -52,9 +50,8 @@ namespace _12_uskovniy_operator
                                 Console.WriteLine("Дух уже призван");
                             }
                             break;
-                        }
+
                     case "3":
-                        {
                             if (isSpiritSummoned == true)
                             {
                                 Console.WriteLine($"Вы нанесли босу {playerAndSpiritDamage} ед. урона. Босс нанес вам {bossDamage} ед. урона.\n Призыв духа завершен.");
@@ -66,11 +63,9 @@ namespace _12_uskovniy_operator
                                 Console.WriteLine("Вы не призвали духа.");
                             }
                             isSpiritSummoned = false;
-
                             break;
-                        }
+    
                     case "4":
-                        {
                             if (numberOfDeparturesInFult > 0)
                             {
                                 Console.WriteLine($"Вы спрятались в разломе и востановили жизнь на {increasedHealthInRift} ед. здоровья.");
@@ -78,23 +73,21 @@ namespace _12_uskovniy_operator
                                 numberOfDeparturesInFult--;
                             }
                             break;
-                        }
                 }
                 Console.Clear();
-
-                if (playerHealth <= 0 && bossHealth <= 0)
-                {
-                    Console.WriteLine("Ничья");
-                }
-                else if (playerHealth <= 0)
-                {
-                    Console.WriteLine("Игрок погиб");
-                }
-                else if (bossHealth <= 0)
-                {
-                    Console.WriteLine("Вы выиграли. Босс погиб");
-                }
-            }            
+            }
+            if (playerHealth <= 0 && bossHealth <= 0)
+            {
+                Console.WriteLine("Ничья");
+            }
+            else if (playerHealth <= 0)
+            {
+                Console.WriteLine("Игрок погиб");
+            }
+            else if (bossHealth <= 0)
+            {
+                Console.WriteLine("Вы выиграли. Босс погиб");
+            }
         }
     }
 }
